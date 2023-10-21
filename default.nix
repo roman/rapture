e5b711ec64da7ad5febb71652709f3b8c7f2439a
Roman Gonzalez <open-source@roman-gonzalez.info>
2023-10-20 23:17:35 -0700

    feat: add nix configuration

---
+++ default.nix
@@ -0,0 +1,30 @@
+inputs: { lib, rsync, stdenv, emacs, coreutils }:
+
+stdenv.mkDerivation {
+  pname   = "rapture";
+  version = "0.1";
+  src = ./.;
+
+  buildInputs = [ emacs coreutils ];
+  buildPhase = ''
+    cp -R $src/* .
+    # tangle org files
+    emacs --batch -Q -l org \
+      --eval '(org-babel-tangle-file "config.org" "config.el")'
+  '';
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out
+    ${rsync}/bin/rsync -av *.el *.org $out
+  '';
+
+  meta = {
+    homepage = "http://github.com/roman/rapture";
+    description = "My doom.d configuration";
+    license = lib.licenses.gpl3;
+    maintainers = [ lib.maintainers.roman ];
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          default = pkgs.callPackage (import ./default.nix inputs) {};
+        };
+      });
+}
