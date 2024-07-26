a922816334b181179f6615db88fa7496fff1aa18
Roman Gonzalez <open-source@roman-gonzalez.info>
2024-07-25 17:48:52 -0700

    feat: add home-manager module

---
+++ nix/modules/home-manager/rapture/default.nix
@@ -0,0 +1,45 @@
+{ self, ... } @ inputs: {lib, pkgs, config, ...}:
+
+let
+  cfg = config.rapture;
+in
+{
+  options.rapture = {
+    enable = lib.mkEnableOption
+      (lib.mdDoc "rapture emacs configuration");
+  };
+
+  config =
+    let
+      emacs = self.packages.${pkgs.system}.emacs;
+    in
+      lib.mkIf (cfg.enable) {
+        programs.emacs = {
+          enable = true;
+          package = emacs;
+        };
+
+        # The bash setup bellow allows vterm buffers to change the
+        # current path of the editor when performing cd commands.
+        programs.bash.initExtra = ''
+          vterm_printf() {
+            if [ -n "$TMUX" ] && ([ "''${TERM%%-*}" = "tmux" ] || [ "''${TERM%%-*}" = "screen" ] ); then
+                # Tell tmux to pass the escape sequences through
+                printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+            elif [ "''${TERM%%-*}" = "screen" ]; then
+                # GNU screen (screen, screen-256color, screen-256color-bce)
+                printf "\eP\e]%s\007\e\\" "$1"
+            else
+                printf "\e]%s\e\\" "$1"
+            fi
+          }
+
+          vterm_prompt_end() {
+            vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+          }
+
+          PROMPT_COMMAND="vterm_prompt_end''${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+        '';
+
+      };
+}
