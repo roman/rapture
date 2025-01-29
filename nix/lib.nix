f5bf9c1b6c9ad1e99ed77570a9e6901f5e4a4ed7
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-01-29 10:04:40 -0800

    feat: add support for plantUML diagrams

--- nix/lib.nix
+++ nix/lib.nix
@@ -21,7 +21,7 @@
 
       runtimeDeps = builtins.attrValues {
         inherit (pkgs)
-          ripgrep gcc gitMinimal;
+          ripgrep gcc gitMinimal plantuml;
 
         inherit (self.packages.${pkgs.system})
           # nix development
