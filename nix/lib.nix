a1332b28c773d190941eb29029589951bea8c13b
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-03-03 19:05:25 -0800

    feat: add git-absorb for magit functionality

--- nix/lib.nix
+++ nix/lib.nix
@@ -21,7 +21,7 @@
 
       runtimeDeps = builtins.attrValues {
         inherit (pkgs)
-          ripgrep gcc gitMinimal plantuml;
+          ripgrep gcc gitMinimal git-absorb plantuml;
 
         inherit (self.packages.${pkgs.system})
           # nix development
