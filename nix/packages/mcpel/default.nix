b6279a3670eae157f6e913e930293dc655dd2670
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-04-25 00:54:25 -0700

    fix: ensure fixed version for mcp.el
    Artifact sha validation was failing.

--- nix/packages/mcpel/default.nix
+++ nix/packages/mcpel/default.nix
@@ -6,7 +6,7 @@ emacsPackages.trivialBuild {
   src = fetchFromGitHub {
     owner = "lizqwerscott";
     repo = "mcp.el";
-    rev = "master";
-    sha256 = "sha256-Lub72Dlzf5SoI6XPK9CM4kxzeTdJKJHdEZ5MGNMduGQ=";
+    rev = "d0f3b5e53c1eded4619e537881e69c4f6d5a2a94";
+    sha256 = "sha256-Clsz6WK0pp4hy9KpN2yEsGEBgyR8Yu6s4H1Pk7rTb/I=";
   };
 }
