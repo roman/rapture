1872dc858fb1d00954d8766b1bead0a22debbd1c
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-05-08 10:47:36 -0700

    refactor: use replaceVars in favor of substituteAll

--- nix/packages/rapture-airbb/default.nix
+++ nix/packages/rapture-airbb/default.nix
@@ -1,8 +1,6 @@
-_inputs: { substituteAll, }:
+_inputs: { replaceVars, }:
 
-substituteAll {
-  name = "config.org";
-  src = ./config.org;
+replaceVars ./config.org {
   # TODO: investigate how to build iap-auth using nix to avoid global dependencies
   # https://git.musta.ch/airbnb/ergo/tree/master/projects/iap-auth
   iapAuthBin = "/usr/local/bin/iap-auth";
