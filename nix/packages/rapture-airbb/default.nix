437b6d293665ef9d5e8b0933d9248f5103a73576
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-05-08 12:12:45 -0700

    refactor: set airchat bin to airtool install path

--- nix/packages/rapture-airbb/default.nix
+++ nix/packages/rapture-airbb/default.nix
@@ -1,10 +1,10 @@
 _inputs: { replaceVars, }:
 
+# TODO: wait for ticket to build package using bazel in nix to avoid global dependencies
+# https://github.com/NixOS/nixpkgs/issues/390395
 replaceVars ./config.org {
-  # TODO: investigate how to build iap-auth using nix to avoid global dependencies
   # https://git.musta.ch/airbnb/ergo/tree/master/projects/iap-auth
   iapAuthBin = "/usr/local/bin/iap-auth";
-  # TODO: wait for ticket to build package using bazel in nix
-  # https://github.com/NixOS/nixpkgs/issues/390395
-  airchatCLI = "/Users/roman_gonzalez/Projects/twig/bazel-bin/projects/airchat/cli/airchat_cli/airchat";
+  # https://git.musta.ch/airbnb/twig/tree/master/projects/airchat/cli/airchat_cli/airchat
+  airchatCLI = "/usr/local/bin/airchat";
 }
