7a6916392a221b03d505b253752a84bf2f9cda2c
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-06-18 13:21:07 -0700

    refactor: remove unnecessary flake inputs

--- flake.nix
+++ flake.nix
@@ -8,22 +8,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixDir.url  = "github:roman/nixDir/roman/fix-inputs-for-lib";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
-
-    pyproject = {
-      url = "github:pyproject-nix/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixDir, ... } @ inputs:
