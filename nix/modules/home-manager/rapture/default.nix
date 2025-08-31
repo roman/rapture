a9129740c79e033d371ff53f8d4d019f24b0795a
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-08-30 20:33:41 -0700

    feat: add home-manager module

---
+++ nix/modules/home-manager/rapture/default.nix
@@ -0,0 +1,66 @@
+{ self, ... }: {lib, pkgs, config, ...}:
+
+let
+  cfg = config.programs.rapture;
+in
+{
+  options.rapture = {
+    enable = lib.mkEnableOption
+      (lib.mdDoc "rapture emacs configuration");
+
+    plugins = lib.mkOption {
+      type = lib.types.listOf [ lib.types.package ];
+      default = [];
+      description = lib.mdDoc "list of rapture plugins to use on this emacs install.";
+    };
+  };
+
+  config =
+    lib.mkIf (cfg.enable) (
+      let
+	emacs = self.packages.${pkgs.system}.rapture.buildEmacs {
+	  inherit (cfg) plugins;
+	};
+      in
+	{
+	  services.emacs = {
+	    enable = true;
+	    package = emacs;
+	    client.enable = true;
+	    client.arguments = [ "-c" ];
+	    defaultEditor = true;
+	  };
+
+          # [editor]: initializing the EDITOR env var to allow git commit messages to work
+          # inside the same emacs session.
+          home.sessionVariables = {
+            EDITOR = lib.mkForce (lib.getBin (pkgs.writeShellScript "editor" ''
+	      exec ${lib.getBin emacs}/bin/emacsclient "''${@:---create-frame}"
+            ''));
+          };
+
+          # The bash setup below allows vterm buffers to change the current path of the
+          # editor when performing cd commands.
+          programs.bash.initExtra = ''
+            vterm_printf() {
+              if [ -n "$TMUX" ] && ([ "''${TERM%%-*}" = "tmux" ] || [ "''${TERM%%-*}" = "screen" ] ); then
+                  # Tell tmux to pass the escape sequences through
+                  printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+              elif [ "''${TERM%%-*}" = "screen" ]; then
+                  # GNU screen (screen, screen-256color, screen-256color-bce)
+                  printf "\eP\e]%s\007\e\\" "$1"
+              else
+                  printf "\e]%s\e\\" "$1"
+              fi
+            }
+
+            vterm_prompt_end() {
+              vterm_printf "51;A$(whoami)@$(hostname):$(pwd)"
+            }
+
+            PROMPT_COMMAND="vterm_prompt_end''${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+          '';
+
+	}
+    );
+}
